2c35b2a5776570239d572d1a91c9358316d489b2
Leander <leander.sabel@gmail.com>
2024-08-01 17:15:38 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+// Codewars testcases use deprecated method rand::Rng::gen
+#![allow(deprecated)]
+
 mod convert_a_string_to_a_number_8kyu;
 mod convert_a_boolean_to_a_string_8kyu;
+mod school_paperwork_8kyu;
 
 fn main() {
     // Not doing anything
