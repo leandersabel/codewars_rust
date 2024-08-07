edce277803753b6bf95f8ab0626ff3a8491b3cff
Leander <leander.sabel@gmail.com>
2024-08-07 21:07:22 +0200

    Solution to "Even or Odd" (8kyu)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod convert_a_string_to_a_number_8kyu;
 mod convert_a_boolean_to_a_string_8kyu;
 mod school_paperwork_8kyu;
 mod invert_values_8kyu;
+mod even_or_odd_8kyu;
 
 fn main() {
     // Not doing anything
